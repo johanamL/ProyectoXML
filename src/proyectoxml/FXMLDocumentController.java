62e21a9ff43b83763f397eb94729cec686f05b23
johana.mosquera <johana.mosquera@SCG-12V-D58736.uao.edu.co>
2018-08-08 14:58:41 -0500

    Cree una interfaz para agregar empleados, con dos botones.

--- src/proyectoxml/FXMLDocumentController.java
+++ src/proyectoxml/FXMLDocumentController.java
@@ -5,12 +5,16 @@
  */
 package proyectoxml;
 
+import Modelo.Empleado;
 import java.net.URL;
+import java.util.LinkedList;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -19,17 +23,64 @@ import javafx.scene.control.Label;
 public class FXMLDocumentController implements Initializable {
     
     @FXML
-    private Label label;
+    private Label ltitulo;
+    @FXML
+    private Label lnombre;
+    @FXML
+    private Label ldir;
+    @FXML
+    private Label ledad;
+    @FXML
+    private Label lcodigo;
+    @FXML
+    private Label ldepto;
     
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    private TextField tnombre;
+    @FXML
+    private TextField tdir;
+    @FXML
+    private TextField tedad;
+    @FXML
+    private TextField tcodigo;
+    @FXML
+    private TextField tdepto;
+    
+    LinkedList<Empleado> listaEmpleados;
+    
+    
+    @FXML
+    private void agregarEmpleadoLista(ActionEvent event) {
+        // System.out.println("You clicked me!");
+        // label.setText("Hello World!");
+        
+        String nombre = tnombre.getText();
+        String direccion = tdir.getText();
+        int edad = Integer.parseInt(tedad.getText());
+        String codigoEmpleado = tcodigo.getText();
+        String departamento = tdepto.getText();
+        
+        Empleado objE = new Empleado(codigoEmpleado, departamento, nombre, direccion, edad);
+        
+        listaEmpleados.add(objE);
+        
+        tnombre.setText("");
+        tdir.setText("");
+        tedad.setText("");
+        tcodigo.setText("");
+        tdepto.setText("");
+        
+        JOptionPane.showMessageDialog(null, "Se agregó un empleado");
+        
+        
+        
     }
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        
+        listaEmpleados = new LinkedList<>();
+        
     }    
     
 }
